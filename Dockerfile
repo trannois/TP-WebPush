323e60a8069c56021ee008693cc286dc7fad338a
trannois <harold.trannois@u-picardie.fr>
2020-10-19 14:06:31 +0200

    Premier déployement Kubernet

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ RUN mkdir -p /opt/project
 COPY src /opt/project/src
 COPY public /opt/project/public
 
-EXPOSE 80
+
+
 
 WORKDIR /opt/project/src
 ENTRYPOINT ["node", "app.js"]
--- js/util.js
+++
@@ -1,9 +0,0 @@
-/**
- * Vérifie que le navigatuer est compatible
- * Possède Push API
- * Return true si c'est ok
- */
-function checkBrowser() {
-    return (('serviceWorker' in navigator) && ('PushManager' in window));
-}
-
